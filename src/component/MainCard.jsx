6d5419f9362e1726528e163b2b8d4550a8e0ffdc
Donghyun Hwang <pointehd@naver.com>
2023-01-24 18:27:45 +0900

    Feat: react-native-image-slider-box 추가

--- src/component/MainCard.jsx
+++ src/component/MainCard.jsx
@@ -1,10 +1,20 @@
-const {View} = require('react-native');
-const {default: UserInfo} = require('./UserInfo');
+import {SliderBox} from 'react-native-image-slider-box';
+import {View} from 'react-native';
+import UserInfo from './UserInfo';
 
-const MainCard = () => {
+const MainCard = props => {
+  const {
+    images = [
+      'https://image.bugsm.co.kr/artist/images/1000/800491/80049126.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-hIkefXesrRZDk78JfnPeJ11hQyA5TW8Hrg&usqp=CAU',
+      require('../asserts/iu.jpeg'),
+    ],
+  } = props;
+
   return (
     <View>
       <UserInfo />
+      <SliderBox images={images} />
     </View>
   );
 };
